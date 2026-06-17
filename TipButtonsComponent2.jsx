f3d32be4228a422f082ebb7589ea43be1be13f51
Andres Chernin <a_cher01@uni-muenster.de>
2026-06-17 08:51:52 +0200

    Upgrate prozent buttons click

--- TipButtonsComponent2.jsx
+++ TipButtonsComponent2.jsx
@@ -3,11 +3,21 @@ function TipButtonsComponent2({errors,values,inputFunction,buttonFunction}){
       <section className="tip-buttons-part">
         <h2>Select Tip %</h2>
         <div className="tip-grid-part">
-            <button onClick={buttonFunction(5)}>5%</button> 
-            <button onClick={buttonFunction(10)}>10%</button> 
-            <button onClick={buttonFunction(15)}>15%</button>
-             <button onClick={buttonFunction(25)}>25%</button>
-              <button onClick={buttonFunction(50)}>50%</button> 
+            <button 
+             className={values.buttonProcents==="5"? "selected":""}
+             onClick={buttonFunction(5)}>5%</button> 
+            <button 
+            className={values.buttonProcents==="10"? "selected":""}
+            onClick={buttonFunction(10)}>10%</button> 
+            <button 
+            className={values.buttonProcents==="15"? "selected":""}
+            onClick={buttonFunction(15)}>15%</button>
+             <button 
+             className={values.buttonProcents==="25"? "selected":""}
+             onClick={buttonFunction(25)}>25%</button>
+              <button 
+              className={values.buttonProcents==="50"? "selected":""}
+              onClick={buttonFunction(50)}>50%</button> 
               <input type="text" 
                      placeholder="Custom" 
                      className="custom-input"
